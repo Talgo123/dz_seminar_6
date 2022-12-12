17cf2e5f43bf7ee49cf1a4e994ea8e5e1dee66fc
talgat <15talgat@mail.ru>
2022-12-12 20:13:09 +0300

    done zadacha_1

---
+++ zadacha_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41:  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+
+
+System.Console.Write("Введите цифры через пробел: ");
+string text = Console.ReadLine();
+string[] result = text.Split(" ");
+CountNumbers(result);
+
+void CountNumbers(string[] text)
+{
+    int i = 0;
+    while (i < result.Length)
+    {
+        i++;
+    }
+    System.Console.WriteLine(i);
+}
